bdc9b853d65a96f4521b6339821338f346288de5
Saqline Shabbir <saqline5869@gmail.com>
2021-10-30 02:12:36 +0600

    changes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mieka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
@@ -15,6 +20,40 @@ async function run(){
       try{
           await client.connect();
           console.log('connected to db')
+          const database = client.db('tourism');
+          const tourCollection =  database.collection('tours')
+          const orderCollection = database.collection('orders8')
+
+      //Get API 
+      app.get('/tours', async (req, res) =>{
+          const cursor = tourCollection.find({});
+          const tours = await cursor.toArray();
+          res.send(tours);
+      })
+
+
+      //Post API
+      app.post('/tours', async(req, res) =>{
+          const tour = req.body
+        console.log('hit the post api', tour);
+        const result = await tourCollection.insertOne(tour)
+        res.json(result);
+      })
+
+    // get api for orders
+    app.get('/orders', async(req, res) =>{
+        const cursor = orderCollection.find({});
+        const orders = await cursor.toArray();
+        res.send(orders)
+    })
+
+    //Post API for orders
+    app.post('/orders', async(req, res) =>{
+        const order = req.body;
+        console.log('hit the order api', order)
+        const result = await orderCollection.insertOne(order)
+        res.json(result)
+    })
 
       }
 
